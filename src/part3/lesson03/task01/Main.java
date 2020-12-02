ce33c8e22d5627cb470d5a7bdc2d8b087e887225
ZaglubotskiyMN <zaglubotskii@gmail.com>
2020-12-02 18:52:22 +0400

    Initial commit

---
+++ src/part3/lesson03/task01/Main.java
@@ -0,0 +1,10 @@
+package part3.lesson03.task01;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int min = 3600;
+	double time = min/60/60;
+    System.out.println(time);
+    }
+}
